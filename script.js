bf072a5a8e5169d894a69b954e1b867a866df1e6
Vaishnavi Singh Rajput <vaishnavirajput981@gmail.com>
2024-02-07 22:51:17 +0530

    uploading files

---
+++ script.js
@@ -0,0 +1,22 @@
+const inputText = document.getElementById("input")
+const button = document.getElementById("button")
+const image = document.getElementById("img")
+const qrImg = document.getElementById("qrImg")
+
+
+function qrCode(event){
+    if(inputText.value === ""){
+        inputText.classList.add('error')
+        setTimeout(() =>{
+            inputText.classList.remove('error')
+        },1000);
+        
+    }
+    else{
+    qrImg.src =  "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + inputText.value; 
+    image.classList.add(".show-img");
+    }
+}
+
+button.addEventListener("click" , qrCode)
+
